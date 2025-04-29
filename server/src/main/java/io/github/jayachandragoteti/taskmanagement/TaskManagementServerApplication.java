082d24b4a273885358256ff01df61579cb2b6641
Jayachandra Goteti <63448352+jayachandragoteti@users.noreply.github.com>
2025-04-29 20:24:07 +0530

    fix: added basic server project

---
+++ server/src/main/java/io/github/jayachandragoteti/taskmanagement/TaskManagementServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.jayachandragoteti.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/io/github/jayachandragoteti/taskmanagement/TaskManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jayachandragoteti.taskmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Temporarily disable logic until full config is set up
+    }
+}
